1f85c203e62468e3af634a885d196edd482fb49f
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-07-12 18:28:31 +0200

    rating added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,15 @@
 @section('content')
     <h1>Movies</h1>
     <div class="container">
-        <div class="row">
+        <div class="row ">
             @foreach($movies as $movie)
-            <div class="col-3 card">
-                {{ $movie -> title}}
+            <div class="p-3 col-5">
+                <div class="card  card-header bg-primary text-light">
+                    <h4>{{ $movie -> title}}</h4>
+                </div>
+                <div class="card-body  bg-dark text-light">
+                    <h6>Rating: </h6> <strong>{{ $movie -> vote}}</strong>
+                </div>
             </div>
             @endforeach
         </div>
